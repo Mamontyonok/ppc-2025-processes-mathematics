b68ea21f7939b189e7f7dc806040b0b47bd2dad7
Mamontyonok <kulikartur05@yandex.ru>
2025-12-13 15:37:09 +0300

    clang-format

--- tasks/kulik_a_star/mpi/src/ops_mpi.cpp
+++ tasks/kulik_a_star/mpi/src/ops_mpi.cpp
@@ -58,56 +58,55 @@ bool KulikAStarMPI::RunImpl() {
   if (proc_num < 3) {
     GetOutput().resize(1u);
     GetOutput()[0] = INT_MAX;
-  }
-  else {
-  auto destination_rank = std::get<1>(input);
-  size_t size = std::get<2>(input).size();
-  int has_data = (size > 0) ? 1 : 0;
-  std::vector<int> all_has_data(proc_num);
-  MPI_Allgather(&has_data, 1, MPI_INT, all_has_data.data(), 1, MPI_INT, MPI_COMM_WORLD);
-  int actual_source = -1;
-  for (int i = 0; i < proc_num; i++) {
-    if (all_has_data[i] == 1) {
-      actual_source = i;
-      break;
-    }
-  }
-  MPI_Bcast(&source_rank, 1, MPI_INT, actual_source, MPI_COMM_WORLD);
-  MPI_Bcast(&destination_rank, 1, MPI_INT, actual_source, MPI_COMM_WORLD);
-  MPI_Bcast(&size, 1, MPI_UINT64_T, actual_source, MPI_COMM_WORLD);
-  GetOutput().resize(size);
-  if (source_rank == destination_rank) {
-    if (proc_rank == source_rank) {
-      GetOutput() = std::get<2>(input);
-    }
-    MPI_Bcast(GetOutput().data(), static_cast<int>(size), MPI_INT, destination_rank, MPI_COMM_WORLD);
   } else {
-    std::vector<int> buff;
-    MPI_Status status;
-
-    if (proc_rank == 0) {
-      buff.resize(size);
-      if (source_rank != 0) {
-        MPI_Recv(buff.data(), static_cast<int>(size), MPI_INT, source_rank, 0, MPI_COMM_WORLD, &status);
-        if (destination_rank != 0) {
-          MPI_Send(buff.data(), static_cast<int>(size), MPI_INT, destination_rank, 0, MPI_COMM_WORLD);
-        } else {
-          GetOutput() = buff;
-        }
-      } else {
-        MPI_Send(std::get<2>(input).data(), static_cast<int>(size), MPI_INT, destination_rank, 0, MPI_COMM_WORLD);
+    auto destination_rank = std::get<1>(input);
+    size_t size = std::get<2>(input).size();
+    int has_data = (size > 0) ? 1 : 0;
+    std::vector<int> all_has_data(proc_num);
+    MPI_Allgather(&has_data, 1, MPI_INT, all_has_data.data(), 1, MPI_INT, MPI_COMM_WORLD);
+    int actual_source = -1;
+    for (int i = 0; i < proc_num; i++) {
+      if (all_has_data[i] == 1) {
+        actual_source = i;
+        break;
       }
-    } else if (proc_rank == destination_rank) {
-      buff.resize(size);
-      if (destination_rank != 0) {
-        MPI_Recv(buff.data(), static_cast<int>(size), MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-      }
-      GetOutput() = buff;
-    } else if (proc_rank == source_rank && source_rank != 0) {
-      MPI_Send(std::get<2>(input).data(), static_cast<int>(size), MPI_INT, 0, 0, MPI_COMM_WORLD);
     }
-    MPI_Bcast(GetOutput().data(), size, MPI_INT, destination_rank, MPI_COMM_WORLD);
-  }
+    MPI_Bcast(&source_rank, 1, MPI_INT, actual_source, MPI_COMM_WORLD);
+    MPI_Bcast(&destination_rank, 1, MPI_INT, actual_source, MPI_COMM_WORLD);
+    MPI_Bcast(&size, 1, MPI_UINT64_T, actual_source, MPI_COMM_WORLD);
+    GetOutput().resize(size);
+    if (source_rank == destination_rank) {
+      if (proc_rank == source_rank) {
+        GetOutput() = std::get<2>(input);
+      }
+      MPI_Bcast(GetOutput().data(), static_cast<int>(size), MPI_INT, destination_rank, MPI_COMM_WORLD);
+    } else {
+      std::vector<int> buff;
+      MPI_Status status;
+
+      if (proc_rank == 0) {
+        buff.resize(size);
+        if (source_rank != 0) {
+          MPI_Recv(buff.data(), static_cast<int>(size), MPI_INT, source_rank, 0, MPI_COMM_WORLD, &status);
+          if (destination_rank != 0) {
+            MPI_Send(buff.data(), static_cast<int>(size), MPI_INT, destination_rank, 0, MPI_COMM_WORLD);
+          } else {
+            GetOutput() = buff;
+          }
+        } else {
+          MPI_Send(std::get<2>(input).data(), static_cast<int>(size), MPI_INT, destination_rank, 0, MPI_COMM_WORLD);
+        }
+      } else if (proc_rank == destination_rank) {
+        buff.resize(size);
+        if (destination_rank != 0) {
+          MPI_Recv(buff.data(), static_cast<int>(size), MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+        }
+        GetOutput() = buff;
+      } else if (proc_rank == source_rank && source_rank != 0) {
+        MPI_Send(std::get<2>(input).data(), static_cast<int>(size), MPI_INT, 0, 0, MPI_COMM_WORLD);
+      }
+      MPI_Bcast(GetOutput().data(), size, MPI_INT, destination_rank, MPI_COMM_WORLD);
+    }
   }
   return true;
 }
--- tasks/kulik_a_star/seq/src/ops_seq.cpp
+++ tasks/kulik_a_star/seq/src/ops_seq.cpp
@@ -56,52 +56,51 @@ bool KulikAStarSEQ::RunImpl() {
   if (proc_num < 3) {
     GetOutput().resize(1u);
     GetOutput()[0] = INT_MAX;
-  }
-  else {
-  auto destination_rank = std::get<1>(GetInput());
-  size_t size = std::get<2>(input).size();
-  int has_data = (size > 0) ? 1 : 0;
-  std::vector<int> all_has_data(proc_num);
-  MPI_Allgather(&has_data, 1, MPI_INT, all_has_data.data(), 1, MPI_INT, MPI_COMM_WORLD);
-  int actual_source = -1;
-  for (int i = 0; i < proc_num; i++) {
-    if (all_has_data[i] == 1) {
-      actual_source = i;
-      break;
-    }
-  }
-  MPI_Bcast(&source_rank, 1, MPI_INT, actual_source, MPI_COMM_WORLD);
-  MPI_Bcast(&destination_rank, 1, MPI_INT, actual_source, MPI_COMM_WORLD);
-  MPI_Bcast(&size, 1, MPI_UINT64_T, actual_source, MPI_COMM_WORLD);
-  GetOutput().resize(size);
-  std::vector<int> index(proc_num, 1);
-  if (!index.empty()) {
-    index[0] = proc_num - 1;
-  }
-  std::vector<int> edges(2 * (proc_num - 1), 0);
-  for (int i = 0; i < proc_num - 1; i++) {
-    edges[i] = i + 1;
-  }
-  MPI_Comm star;
-  MPI_Graph_create(MPI_COMM_WORLD, proc_num, index.data(), edges.data(), 0, &star);
-  if (source_rank == destination_rank) {
-    if (proc_rank == source_rank) {
-      GetOutput() = std::get<2>(input);
-    }
-    MPI_Bcast(GetOutput().data(), static_cast<int>(size), MPI_INT, destination_rank, star);
   } else {
-    std::vector<int> buff;
-    MPI_Status status;
-    if (proc_rank == destination_rank) {
-      buff.resize(size);
-      MPI_Recv(buff.data(), static_cast<int>(size), MPI_INT, source_rank, MPI_ANY_TAG, star, &status);
-    } else if (proc_rank == source_rank) {
-      MPI_Send(std::get<2>(input).data(), static_cast<int>(size), MPI_INT, destination_rank, 0, star);
+    auto destination_rank = std::get<1>(GetInput());
+    size_t size = std::get<2>(input).size();
+    int has_data = (size > 0) ? 1 : 0;
+    std::vector<int> all_has_data(proc_num);
+    MPI_Allgather(&has_data, 1, MPI_INT, all_has_data.data(), 1, MPI_INT, MPI_COMM_WORLD);
+    int actual_source = -1;
+    for (int i = 0; i < proc_num; i++) {
+      if (all_has_data[i] == 1) {
+        actual_source = i;
+        break;
+      }
     }
-    MPI_Bcast(GetOutput().data(), static_cast<int>(size), MPI_INT, destination_rank, star);
+    MPI_Bcast(&source_rank, 1, MPI_INT, actual_source, MPI_COMM_WORLD);
+    MPI_Bcast(&destination_rank, 1, MPI_INT, actual_source, MPI_COMM_WORLD);
+    MPI_Bcast(&size, 1, MPI_UINT64_T, actual_source, MPI_COMM_WORLD);
+    GetOutput().resize(size);
+    std::vector<int> index(proc_num, 1);
+    if (!index.empty()) {
+      index[0] = proc_num - 1;
+    }
+    std::vector<int> edges(2 * (proc_num - 1), 0);
+    for (int i = 0; i < proc_num - 1; i++) {
+      edges[i] = i + 1;
+    }
+    MPI_Comm star;
+    MPI_Graph_create(MPI_COMM_WORLD, proc_num, index.data(), edges.data(), 0, &star);
+    if (source_rank == destination_rank) {
+      if (proc_rank == source_rank) {
+        GetOutput() = std::get<2>(input);
+      }
+      MPI_Bcast(GetOutput().data(), static_cast<int>(size), MPI_INT, destination_rank, star);
+    } else {
+      std::vector<int> buff;
+      MPI_Status status;
+      if (proc_rank == destination_rank) {
+        buff.resize(size);
+        MPI_Recv(buff.data(), static_cast<int>(size), MPI_INT, source_rank, MPI_ANY_TAG, star, &status);
+      } else if (proc_rank == source_rank) {
+        MPI_Send(std::get<2>(input).data(), static_cast<int>(size), MPI_INT, destination_rank, 0, star);
+      }
+      MPI_Bcast(GetOutput().data(), static_cast<int>(size), MPI_INT, destination_rank, star);
+    }
+    MPI_Comm_free(&star);
   }
-  MPI_Comm_free(&star);
-}
   return true;
 }
 
